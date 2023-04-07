05de5880c6fce3dbd23e3f82085b5d657b0c5c03
Vishal6300 <vkdeoghar1998@gmail.com>
2023-04-07 14:56:41 +0530

    added Connection file

--- day1_Ques1/src/main/java/jdbc/JDBC.java
+++ day1_Ques1/src/main/java/jdbc/JDBC.java
@@ -1,15 +1,19 @@
 package jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class JDBC {
 	
 	public Connection getConnection() throws Exception{
 		
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		String url="jdbc:mysql://localhost:3306/SB201";
 		String username="root";
 		String password="root";
 		
+		Connection conn= DriverManager.getConnection(url,username,password);
 		return conn;
 	}
 }
