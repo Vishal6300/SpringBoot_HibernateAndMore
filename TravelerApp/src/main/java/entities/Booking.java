b6b4a9d8ea9c2d3ca785c4afeaa46b41e9d6816e
Vishal6300 <vkdeoghar1998@gmail.com>
2023-04-14 22:49:11 +0530

    added some changes

--- TravelerApp/src/main/java/entities/Booking.java
+++ TravelerApp/src/main/java/entities/Booking.java
@@ -1,5 +1,12 @@
 package entities;
 
+import java.time.LocalDate;
+
 public class Booking {
+	private int bookingId;
+	private String travelerName;
+	private String destination;
+	private LocalDate bookingDate;
+	private TravelPackage travelPackage;
 
 }
--- TravelerApp/src/main/java/entities/TravelPackage.java
+++ TravelerApp/src/main/java/entities/TravelPackage.java
@@ -1,5 +1,23 @@
 package entities;
 
-public class TravelPackage {
+import java.util.Set;
 
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class TravelPackage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int packageId;
+	private String packageName;
+	private double price;
+	private Set<Booking> bookings;
+/*
+ packageId : int
+	packageName : String
+	price : double
+	bookings : Set<Booking>
+ */
 }
