b7c8488321bfd1b105e80fcf7254904b82a952f6
Vishal6300 <vkdeoghar1998@gmail.com>
2023-04-14 22:36:18 +0530

    added some changes

--- TravelerApp/src/main/java/utilities/EMUtil.java
+++ TravelerApp/src/main/java/utilities/EMUtil.java
@@ -1,5 +1,21 @@
 package utilities;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class EMUtil {
+    private static EntityManagerFactory emf;
+    private static final String PERSISTENCE_UNIT_NAME = "TravelerApp";
+
+    static {
+    	emf=Persistence.createEntityManagerFactory(null);
+    }
     
+    public static EntityManager provideEntityManager() {
+    	if(emf==null) {
+    		emf= Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    	}
+    	return emf.createEntityManager();
+    }
 }
