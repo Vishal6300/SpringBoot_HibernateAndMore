95d951f0303b991d186817b7b3a575c1b262ae82
Vishal6300 <vkdeoghar1998@gmail.com>
2023-04-14 22:53:23 +0530

    added some changes

--- TravelerApp/src/main/java/entities/TravelPackage.java
+++ TravelerApp/src/main/java/entities/TravelPackage.java
@@ -2,16 +2,18 @@ package entities;
 
 import java.util.Set;
 
-
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 public class TravelPackage {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int packageId;
 	private String packageName;
+	@OneToMany(cascade = CascadeType.ALL)
 	private double price;
 	private Set<Booking> bookings;
 /*
