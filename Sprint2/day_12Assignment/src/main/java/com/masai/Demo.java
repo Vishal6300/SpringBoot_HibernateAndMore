15c786a3c8cf0c14201b3f7293f3068772d8c9fd
Vishal6300 <vkdeoghar1998@gmail.com>
2023-04-23 22:44:22 +0530

    added packages and classes

--- Sprint2/day_12Assignment/src/main/java/com/masai/Demo.java
+++ Sprint2/day_12Assignment/src/main/java/com/masai/Demo.java
@@ -5,9 +5,14 @@ import java.util.Map;
 public class Demo {
 	private Map<Department, Employee> theMap;
 	//use setter injection to inject theMap;
+	
 
 	public void myInit(){
 		System.out.println("inside myInit method");
+	}
+
+	public void setTheMap(Map<Department, Employee> theMap) {
+		this.theMap = theMap;
 	}
 
 	public void cleanUp(){
@@ -17,5 +22,8 @@ public class Demo {
 	public void showDetails(){
 		System.out.println("inside showDetails of Demo class");
 		//print all the details of all the employees department-wise.
+		for (Map.Entry<Department, Employee> entry : theMap.entrySet()) {
+			System.out.println(entry.getKey() + " " + entry.getValue());
+		}
 	}
 }
---
+++ Sprint2/day_12Assignment/src/main/java/com/masai/Main.java
@@ -0,0 +1,15 @@
+package com.masai;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ctx= new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Demo demo= (Demo) ctx.getBean("demo");
+	}
+
+}
