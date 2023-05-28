cc171c64ad72e61813ae0b5754dd895a8648a3da
Vishal6300 <vkdeoghar1998@gmail.com>
2023-05-29 00:49:26 +0530

    added main class

--- CRUDOperationUsingSpringBoot/src/main/java/com/masai/demo/CrudOperationUsingSpringBootApplication.java
+++ CRUDOperationUsingSpringBoot/src/main/java/com/masai/demo/CrudOperationUsingSpringBootApplication.java
@@ -2,12 +2,26 @@ package com.masai.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.masai.demo.dao.UserRepository;
+import com.masai.demo.model.User;
 
 @SpringBootApplication
 public class CrudOperationUsingSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudOperationUsingSpringBootApplication.class, args);
+		ApplicationContext context= SpringApplication.run(CrudOperationUsingSpringBootApplication.class, args);
+		UserRepository userRepository= context.getBean(UserRepository.class);
+		
+		User user1= new User();
+		
+		user1.setName("Vivek kumar");
+		user1.setCity("Dehradun");
+		user1.setStatus("Java Developer");
+		
+		User user= userRepository.save(user1);
+		
 	}
 
 }
