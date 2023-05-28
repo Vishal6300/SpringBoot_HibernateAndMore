94b5b0bffb69c2fd4a95489430d871682fe81c91
Vishal6300 <vkdeoghar1998@gmail.com>
2023-05-28 23:57:35 +0530

    added User class

--- CRUDOperationUsingSpringBoot/src/main/java/com/masai/model/User.java
+++ CRUDOperationUsingSpringBoot/src/main/java/com/masai/model/User.java
@@ -15,4 +15,38 @@ public class User {
 	private String city;
 	
 	private String status;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 }
